8b141f006b21e84519938877f0d36b4e163521d7
letusgitcommit <davidvaughn006@gmail.com>
2023-01-12 21:12:20 -0600

    Wrote implemenation for formMessage function

--- main.js
+++ main.js
@@ -6,7 +6,14 @@ function pickRandomMessage(messageArray) {
 }
 
 function formMessage(messageArray){
+    const messageArrayCopy = [...messageArray];
 
+    const capitalizedFirstCharacterOfFirstMessage = messageArrayCopy[0][0].toUpperCase();
+    const remainingMessage = messageArrayCopy[0].slice(1);
+    messageArrayCopy[0] = capitalizedFirstCharacterOfFirstMessage + remainingMessage;
+
+    const message = messageArrayCopy.join(' ') + '.';
+    return message;
 }
 
 module.exports = {
