d882413a9b2fdc39a5952a821409ae560eb5b093
letusgitcommit <davidvaughn006@gmail.com>
2023-01-12 21:20:17 -0600

    implemented a main function to achieve desired functionality

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-console.log('message');
-
 function pickRandomMessage(messageArray) {
     const randomIndex = Math.floor(Math.random() * messageArray.length);
     return messageArray[randomIndex];
@@ -16,7 +14,48 @@ function formMessage(messageArray){
     return message;
 }
 
+function main() {
+
+    const subjectPhrases = [
+        'the sun',
+        'the moon',
+        'a star',
+        'someone',
+        'that computer',
+        'the lamp',
+    ]
+
+    const verbPhrases = [
+        'is',
+        'is not',
+        'totally did',
+        'moved away from',
+        'leaped over',
+        'bounded through',
+    ]
+
+    const adjectivePhrases = [
+        'so pink',
+        'so blue',
+        'very noticeable',
+        'invisible',
+        'tasty',
+    ]
+
+    const phrases = [
+        pickRandomMessage(subjectPhrases),
+        pickRandomMessage(verbPhrases),
+        pickRandomMessage(adjectivePhrases)
+    ]
+
+    const message = formMessage(phrases);
+
+    console.log(message);
+}
+
 module.exports = {
     pickRandomMessage: pickRandomMessage,
     formMessage: formMessage,
 }
+
+main();
