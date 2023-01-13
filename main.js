55cce5b04d0a115c053ef8059835ac1e3b6cdcdc
letusgitcommit <davidvaughn006@gmail.com>
2023-01-12 20:47:24 -0600

    Created a randomMessagePicker

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 console.log('message');
+
+function randomMessagePicker(message_array) {
+    const random_index = Math.floor(Math.random() * message_array.length);
+    return message_array[random_index];
+}
+
+module.exports = {
+    randomMessagePicker: randomMessagePicker,
+}
